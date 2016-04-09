3a0f015c688d9748814c591af68581f908173423
Mickey Barboi <damouse007@gmail.com>
2016-04-09 17:13:06 -0500

    added reflection code

--- core/core.go
+++ core/core.go
@@ -28,3 +28,43 @@ func main() {
 		}()
 	}
 }
+package core
+
+// Provides a generalized interface into all Core functions
+// Relies on github.com/damouse/pgkreflect to dynamically check calls and types
+
+type session struct {
+    mem map[uint64]interface{} // "heap" space for this session
+}
+
+// Setting variables
+func (s *session) Set(key string, value []interface{}) {
+
+}
+
+func (s *session) Get(key string) {
+
+}
+
+// Invoke the given function with args and callback ids. If the function can and does
+// succeed, cb is called back with the results. If the function returns a non-nil error,
+// eb is invoked with a string
+func (s *session) InvokeDeferred(name string, cb int64, eb int64, args []interface{}) {
+
+}
+
+// Maps a JSON to a struct matched by a key?
+// DeferredReturn: uint64 pointer
+func (s *session) Alloc(key string, args []interface{}) {
+
+}
+
+func (s *session) Free(ptr uint64) {
+
+}
+
+// Wraps a function in a deferred. If the function returns a non-nil error, callback eb
+// with the string resut. If the function returns anything else then call it back
+func (s *session) WrapDeferred(cb uint64, eb uint64) {
+
+}
