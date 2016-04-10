480e3a8d817bd073fcff95e5879b990c829384eb
damouse <damouse007@gmail.com>
2016-04-09 19:04:15 -0500

    some application issues, but otherwise ok

--- core/core.go
+++ core/core.go
@@ -6,35 +6,102 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"net"
+	"reflect"
+
+	"github.com/exis-io/core"
 )
 
 func main() {
 	listener, _ := net.Listen("tcp", ":9876")
-	fmt.Println("Server started. Reflected package interface: ")
+	fmt.Println("core started")
+
+	methods := make(map[string]map[string]reflect.Method)
+
+	// Build the list of methods available
+	for k, v := range core.Types {
+		e := make(map[string]reflect.Method)
+
+		for i := 0; i < v.NumMethod(); i++ {
+			m := v.Method(i)
+			e[m.Name] = m
+		}
+
+		methods[k] = e
+	}
+
+	var sessions []*session
 
 	for {
 		conn, _ := listener.Accept()
-		fmt.Println("Someone connected")
 		writer := bufio.NewWriter(conn)
 		reader := bufio.NewReader(conn)
 
-		go func() {
-			line, _ := reader.ReadString('\n')
-			fmt.Println("Received: ", line)
-			writer.WriteString("Go: Hey client!\n")
-			writer.Flush()
-		}()
+		s := &session{
+			mem:    make(map[uint64]interface{}),
+			conn:   &conn,
+			writer: writer,
+			reader: reader,
+		}
+
+		sessions = append(sessions, s)
+		go s.run()
 	}
 }
-package core
 
 // Provides a generalized interface into all Core functions
 // Relies on github.com/damouse/pgkreflect to dynamically check calls and types
 
+type invocation struct {
+	target string
+	cb     float64
+	eb     float64
+	args   []interface{}
+}
+
 type session struct {
-    mem map[uint64]interface{} // "heap" space for this session
+	mem    map[uint64]interface{} // "heap" space for this session
+	conn   *net.Conn
+	writer *bufio.Writer
+	reader *bufio.Reader
+}
+
+func (s *session) run() {
+	for {
+		line, _ := s.reader.ReadBytes('\n')
+
+		var d []interface{}
+		if e := json.Unmarshal(line, &d); e != nil {
+			fmt.Printf("Unable to unmarshall data: %s\n", e)
+		}
+
+		n := &invocation{
+			target: d[0].(string),
+			cb:     d[1].(float64),
+			eb:     d[2].(float64),
+			args:   d[3].([]interface{}),
+		}
+
+		fmt.Println("Received: ", n)
+		// s.writer.WriteString("Go: Hey client!\n")
+		// s.writer.Flush()
+
+		if m, ok := core.Variables[n.target]; ok {
+			fmt.Println("Variable", m)
+		} else if m, ok := core.Types[n.target]; ok {
+			fmt.Println("Types", m)
+		} else if m, ok := core.Consts[n.target]; ok {
+			fmt.Println("Consts", m)
+		} else if m, ok := core.Functions[n.target]; ok {
+			fmt.Printf("Functions %v\n", m)
+			r := m.Call(n.args)
+			fmt.Printf("Result: %v\n", r)
+		} else {
+			fmt.Println("Unknown!")
+		}
+	}
 }
 
 // Setting variables
@@ -53,6 +120,10 @@ func (s *session) InvokeDeferred(name string, cb int64, eb int64, args []interfa
 
 }
 
+func (s *session) Invoke(name string, args []interface{}) {
+
+}
+
 // Maps a JSON to a struct matched by a key?
 // DeferredReturn: uint64 pointer
 func (s *session) Alloc(key string, args []interface{}) {
@@ -60,11 +131,5 @@ func (s *session) Alloc(key string, args []interface{}) {
 }
 
 func (s *session) Free(ptr uint64) {
-
-}
-
-// Wraps a function in a deferred. If the function returns a non-nil error, callback eb
-// with the string resut. If the function returns anything else then call it back
-func (s *session) WrapDeferred(cb uint64, eb uint64) {
 
 }
--- java/app/src/main/java/com/ironether/client/Main.java
+++ java/app/src/main/java/com/ironether/client/Main.java
@@ -1,13 +1,22 @@
 package com.ironether.client;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * This is the "client"
@@ -15,6 +24,8 @@ import java.net.Socket;
  *  - type safe
  *  - "static" wrt function calls, by dynamic wrt the exposed functionality
  *  - asynchronous
+ *
+ *  The core only calls us with Deferred style callbacks- (uint64, uint64, args)
  */
 public class Main {
     public static void log(String s) {
@@ -25,9 +36,14 @@ public class Main {
         log("Starting the client");
 
         MrsFrizzle f = new MrsFrizzle("localhost", 9876);
-        f.send("Java: Hi!\n");
 
-        f.run();
+        f.affect("Info", "This is Patrick");
+
+        try {
+            f.thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
@@ -37,8 +53,13 @@ class MrsFrizzle {
     DataOutputStream out;
     Thread thread;
 
+    Gson gson = new Gson();
+    Map<Integer, Deferred> deferreds =  new HashMap();
+
+
     MrsFrizzle(String host, int port) {
         Main.log("Connecting to " + host + " " + port);
+
         try {
             Socket socket = new Socket(InetAddress.getByName(host), port);
             in = new DataInputStream(socket.getInputStream());
@@ -46,49 +67,120 @@ class MrsFrizzle {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        listen();
     }
 
     // Spin and listen for events
     public void listen() {
         thread = new Thread() {
             public void run() {
-                    run();
+                BufferedReader d = new BufferedReader(new InputStreamReader(in));
+
+                while (true) {
+                    try {
+                        String line = d.readLine();
+
+                        if (line == null)
+                            break;
+
+                        System.out.println("Java has: " + line);
+
+                    } catch (Exception e) {
+                        Main.log("Things didnt go well");
+                        e.printStackTrace();
+                    }
+                }
+
+                Main.log("Connection lost");
                 }
 
         };
         thread.start();
     }
 
-    // Spin and listen
-    public void run() {
-        BufferedReader d = new BufferedReader(new InputStreamReader(in));
-
-        while (true) {
-            try {
-                String line = d.readLine();
-
-                if (line == null)
-                    break;
-
-                System.out.println("Java has: " + line);
-
-            } catch (Exception e) {
-                Main.log("Things didnt go well");
-                e.printStackTrace();
-            }
-        }
-
-        Main.log("Connection lost");
-    }
 
     public void send(String response) {
         Main.log("Writing " + response);
 
         try {
-            out.writeBytes(response);
+            out.writeBytes(response + "\n");
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // Affect some change in the core. The result of the operation depends on what the type of the thing is.
+    // Types can be instantiated and return a pointer
+    // Functions can be invoked
+    // Variables can be set and read
+    // Constants can be read
+    // Always returns a Deferred. The deferred is only invoked if its callback methods are called
+    public Deferred affect(String target, Object... args) {
+        List<Object> r = new ArrayList();
+        Deferred d = new Deferred();
+        r.add(target);
+        r.add(d.cb);
+        r.add(d.eb);
+        r.add(args);
+        send(gson.toJson(r));
+
+        return d;
+    }
+}
+
+class Deferred {
+    int cb;
+    int eb;
+    Function _callback;
+    Function _errback;
+
+    private static Random gen = new Random();
+
+    public Deferred() {
+        cb = gen.nextInt();
+        eb = gen.nextInt();
+    }
+
+
+    // We will need to override cuminicable methods here again, much like swift
+    Deferred _then(Function fn) {
+        _callback = fn;
+        return this;
+    }
+
+    Deferred _error(Function fn) {
+        _errback = fn;
+        return this;
+    }
+
+    void callback(Object[] args) {
+        if (_callback != null) {
+            _callback.invoke(args);
+        }
+    }
+
+    void errback(Object[] args) {
+        if (_errback != null) {
+            _errback.invoke(args);
+        }
+    }
+
+    //
+    // Generic Shotgun
+    //
+
+    // No args
+    public Deferred then(Function handler) {
+        return _then(handler);
+    }
+
+    public Deferred error(Function handler) {
+        return _error(handler);
+    }
+}
+
+interface Function {
+    void invoke(Object[] args);
 }
